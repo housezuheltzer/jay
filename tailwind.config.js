10ed834802a07e41d8ccf329975b4a4312d32df3
Jovan Petrovic <jovan@Jovans-Mac-mini.local>
2025-04-24 18:57:01 +1000

    Fix Tailwind content configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html", // if using index.html
+    "./src/**/*.{js,ts,jsx,tsx}", // if using React, JSX, or TSX files
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
